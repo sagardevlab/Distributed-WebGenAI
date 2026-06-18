0c64083feb97289ad418620a217ce9a76b4ad18a
sagardevlab <sagar.dev.lab@gmail.com>
2026-06-18 16:02:10 +0530

    API Gateway service implemented

---
+++ api_gateway/src/test/java/com/sagardevlab/distributed_webgenai/api_gateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sagardevlab.distributed_webgenai.api_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
